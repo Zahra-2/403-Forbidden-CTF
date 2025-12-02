1d356d6e25aae679d1455766b836698d502f64d6
Zahra-2 <121816188+Zahra-2@users.noreply.github.com>
2025-12-02 14:56:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,7 @@ def noise():
 # ----------------------------
 import os
 
-if name == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
+
